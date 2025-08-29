628810858520e215a156734b2011a6f14ba187c0
Kendeki <92603580+Kendeki@users.noreply.github.com>
2025-08-29 18:04:20 -0300

    modificação para acessar os dados a partir de json
    implementei a logica para acesso a API no dataFormer.py e depositei os resultados no data.ndjson. modifiquei model.py para melhor utilização dos dados recebidos

--- dataFormer.py
+++ dataFormer.py
@@ -1,51 +1,46 @@
 import requests as r
 import json as j
-from datetime import date, timedelta
+from time import sleep
 from random import uniform
-coords = [(round(uniform(-90, 90), 6), 
-                round(uniform(-180, 180), 6)) for _ in range(30)]
 
+coords = [(round(uniform(-90, 90), 6), 
+                round(uniform(-180, 180), 6)) for _ in range(50)]
+
+url = "https://archive-api.open-meteo.com/v1/archive"
 params = {
     "latitude": 0,
     "longitude": 0,
-    "start_date": date.today() - timedelta(days=1),
-    "end_date": str(date.today() - timedelta(days=730)),
+    "start_date":"",
+    "end_date": "",
     "timezone": "America/Sao_Paulo",
-    "daily": ["weather_code", "cape_mean", "dew_point_2m_mean", 
-            "pressure_msl_mean", "surface_pressure_mean", "temperature_2m_max", 
-            "apparent_temperature_max", "apparent_temperature_min", "wind_speed_10m_max", 
-            "wind_gusts_10m_max", "wind_direction_10m_dominant", "shortwave_radiation_sum", 
-            "daylight_duration", "sunshine_duration", "uv_index_max", "uv_index_clear_sky_max", 
-            "precipitation_hours", "precipitation_probability_max", "precipitation_sum", 
-            "snowfall_sum", "showers_sum", "rain_sum", "cape_min", "cape_max", 
-            "temperature_2m_mean", "temperature_2m_min", "apparent_temperature_mean", 
-            "dew_point_2m_max", "dew_point_2m_min", "cloud_cover_min", "cloud_cover_max", 
-            "cloud_cover_mean"],
+    "daily": ["weather_code", "dew_point_2m_mean", "pressure_msl_mean", "surface_pressure_mean",
+            "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant", "shortwave_radiation_sum",
+            "daylight_duration", "sunshine_duration", "precipitation_hours",
+            "precipitation_sum", "snowfall_sum", "showers_sum", "rain_sum", "temperature_2m_mean",
+            "apparent_temperature_mean", "cloud_cover_mean"]
 }
 
-def create_json():
-    for (lat, lon) in coords:
+def create_json(start_date="2023-01-01", end_date="2024-01-01"):
+    params["start_date"] = start_date
+    params["end_date"] = end_date
+
+    for lat, lon in coords:
+        sleep(10)
         params["latitude"] = lat
         params["longitude"] = lon
-        url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
-        response = r.get(url=url, params=params).json()
-
         try:
-            if response['error']:
-                continue
-        except KeyError:
-            with open("data.ndjson", 'a') as f:
-                f.write(j.dumps(response) + '\n')
+            response = r.get(url=url, params=params).json()
+        except:
+            continue
+        if response.get("error", False):
+            print(f"Erro de gravação com as coordenadas ({lat}, {lon}).")
+            print(response['reason'])
+            continue
 
-
-
-def checkToday():
-        url = "https://api.open-meteo.com/v1/forecast"
-        params["start_date"] = str(date.today())
-        params["end_date"] = params["start_date"]
-
+        print(f"Gravação concluída com as coordenadas ({lat}, {lon})")
         with open("data.ndjson", 'a') as f:
             f.write(j.dumps(response) + '\n')
 
-
-create_json()
+if __name__ == '__main__':
+    create_json()
+    create_json(start_date="2024-01-02", end_date="2025-01-01")
--- model.py
+++ model.py
@@ -1,28 +1,27 @@
-import requests as r
+import json
 import pandas as pd
-from datetime import date, timedelta
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import root_mean_squared_error, precision_score
 
-def models(lat:float = 0, lon:float = 0):
+def models():
 
     def group_weather(code):
         if code == 0:
             return 0
         elif code in [1,2,3]:
-            return 1  
+            return 1
         elif code in [45,48]:
-            return 2  
+            return 2
         elif code in [51,53,55,56,57]:
-            return 3  
+            return 3
         elif code in [61,63,65,66,67,80,81,82]:
-            return 4  
+            return 4
         elif code in [71,73,75,77,85,86]:
-            return 5  
+            return 5
         elif code in [95,96,99]:
             return 6
-    
+
     weather_code = {
         0: "Clear Sky",
         1: "Cloudy",
@@ -33,27 +32,48 @@ def models(lat:float = 0, lon:float = 0):
         6: "Thunderstorm"
     }
 
-    data = pd.DataFrame(response['daily'], index=response["daily"]["time"])
-    data = pd.concat(data, pd.DataFrame(response['daily'], index=['daily']['time']))
-    
-    data = data.drop(columns=["time"])
+    data_list = []
+
+    with open("data.ndjson", "r", encoding="utf-8") as f:
+        for line in f:
+            record = json.loads(line)
+            lat    = record["latitude"]
+            lon    = record["longitude"]
+            daily  = record["daily"]
+            data_temp = pd.DataFrame(daily)
+
+            data_temp["latitude"]  = lat
+            data_temp["longitude"] = lon
+            data_list.append(data_temp)
+
+    data = pd.concat(data_list, ignore_index=True)
+
+    data["time"]        = pd.to_datetime(data["time"])
+    data['day_of_year'] = data['time'].dt.dayofyear
+    data['month']       = data['time'].dt.month
+    data['weekday']     = data['time'].dt.weekday
+    data['year']        = data['time'].dt.year
+
+    data = data.sort_values(["latitude", "longitude", "time"])
+    data = data.drop(columns="time")
     data["weather_code"] = data["weather_code"].apply(group_weather)
-    length = len(data)
 
     for column in data:
         if column == "weather_code":
             continue
-        data[column+'1'] = data[column].shift(1)
-        data[column+'2'] = data[column].shift(2)
+        data[column+'1'] = data.groupby(["latitude", "longitude"])[column].shift(1)
+        data[column+'2'] = data.groupby(["latitude", "longitude"])[column].shift(2)
+
     data = data.fillna(0)
+    data = data.dropna()
+    data = data.replace({None: 0})
 
     size_train = int(data.shape[0]*0.8)
 
     # /----- Temperature Prediction -----/
 
-    temperatures = ["temperature_2m_max", "apparent_temperature_max", "apparent_temperature_min", 
-                    "temperature_2m_mean", "temperature_2m_min", "apparent_temperature_mean"]
-    
+    temperatures = ["temperature_2m_mean"]
+
     modelTemp = LinearRegression()
 
     x_train = data.iloc[:size_train, :].drop(columns=temperatures)
@@ -63,22 +83,23 @@ def models(lat:float = 0, lon:float = 0):
 
     modelTemp.fit(x_train, y_train)
     predT = modelTemp.predict(x_test)
-    rmce = [root_mean_squared_error(predT[:, i], y_test.iloc[:, i]) 
-            for i in range(len(temperatures))]
+    rmce = root_mean_squared_error(predT[:, 0], y_test.iloc[:, 0])
+
     # /----- Weather Prediction -----/
 
-    counts = data["weather_code"].value_counts()
+    counts  = data["weather_code"].value_counts()
     weights = {weight: counts.sum()/count for weight, count in counts.items()}
+
     modelWeather = RandomForestClassifier(class_weight=weights, random_state=67)
 
-    x_train = data.iloc[:size_train, length:]
-    x_test  = data.iloc[size_train:, length:]
+    x_train = data.iloc[:size_train, 1:]
+    x_test  = data.iloc[size_train:, 1:]
     y_train = data.iloc[:size_train, 0]
     y_test  = data.iloc[size_train:, 0]
 
     modelWeather.fit(x_train, y_train)
     predW = modelWeather.predict(x_test)
-    prc = precision_score(predW, y_test)
+    prc   = precision_score(predW, y_test, average='weighted')
 
     return {"temperatures": predT[-1], 
             "margin": rmce, 
@@ -88,6 +109,6 @@ def models(lat:float = 0, lon:float = 0):
 def main():
     results = models()
     print(results)
-    
+
 if __name__ == '__main__':
     main()
