e0a7008c6af8b034e8aad21a9ca6e01e52c8baac
Kendeki <92603580+Kendeki@users.noreply.github.com>
2025-09-09 23:10:52 -0300

    versao funcional (?)

--- dataFormer.py
+++ dataFormer.py
@@ -7,7 +7,6 @@ from random import uniform
 coords = [(round(uniform(-90, 90), 6), 
                 round(uniform(-180, 180), 6)) for _ in range(50)]
 
-url = "https://archive-api.open-meteo.com/v1/archive"
 params = {
     "latitude": 0,
     "longitude": 0,
@@ -21,7 +20,9 @@ params = {
             "apparent_temperature_mean", "cloud_cover_mean"]
 }
 
-def create_json(start_date="2023-01-01", end_date="2024-01-01"):
+def create_json(start_date=str(date.today() - timedelta(days=730)), 
+                end_date=str(date.today() - timedelta(days=365))):
+    url = "https://archive-api.open-meteo.com/v1/archive"
     params["start_date"] = start_date
     params["end_date"] = end_date
 
@@ -44,6 +45,7 @@ def create_json(start_date="2023-01-01", end_date="2024-01-01"):
             f.write(j.dumps(response) + '\n')
 
 def user_test_data(lat: float, lon: float):
+    url = "https://archive-api.open-meteo.com/v1/archive"
     params["latitude"] = lat
     params["longitude"] = lon
     params["start_date"] = str(date.today() - timedelta(days=365))
@@ -64,7 +66,7 @@ def tomorrows_prediction_data(lat: float, lon: float):
     url = "https://api.open-meteo.com/v1/forecast"
     params["latitude"] = lat
     params["longitude"] = lon
-    params["start_date"] = str(date.today())
+    params["start_date"] = str(date.today() - timedelta(days=2))
     params["end_date"] = str(date.today())
 
     try:
@@ -80,4 +82,3 @@ def tomorrows_prediction_data(lat: float, lon: float):
     
 if __name__ == '__main__':
     create_json()
-    create_json(start_date="2024-01-02", end_date="2025-01-01")
--- model.py
+++ model.py
@@ -47,12 +47,13 @@ def models(latitude: float, longitude: float):
             data_temp["latitude"]  = lat
             data_temp["longitude"] = lon
             data_list.append(data_temp)
+
     data = pd.concat(data_list, ignore_index=True)
 
     prediction_data = tomorrows_prediction_data(latitude, longitude)
     prediction_test = user_test_data(latitude, longitude)
 
-    if prediction_data:
+    if prediction_data and prediction_test:
         lat    = prediction_data["latitude"]
         lon    = prediction_data["longitude"]
         daily  = prediction_data["daily"]
@@ -60,13 +61,8 @@ def models(latitude: float, longitude: float):
 
         prediction_list["latitude"]  = lat
         prediction_list["longitude"] = lon
+        prediction_data = prediction_list.copy()
 
-    else:
-        raise RuntimeError(f'Dados indisponíves, tente novamente.')
-    prediction_data = prediction_list.copy()
-    prediction_list = []
-    
-    if prediction_test:
         lat    = prediction_test["latitude"]
         lon    = prediction_test["longitude"]
         daily  = prediction_test["daily"]
@@ -74,14 +70,12 @@ def models(latitude: float, longitude: float):
 
         prediction_list["latitude"]  = lat
         prediction_list["longitude"] = lon
+        prediction_test = prediction_list.copy()
+
     else:
         raise RuntimeError(f'Dados indisponíves, tente novamente.')
-    prediction_test = prediction_list.copy()
 
-    del data_list
-    del prediction_list
-
-    for dataframe in (data, prediction_data):
+    for dataframe in (data, prediction_data, prediction_test):
         dataframe["time"]        = pd.to_datetime(dataframe["time"])
         dataframe["day_of_year"] = dataframe["time"].dt.dayofyear
         dataframe["month"]       = dataframe["time"].dt.month
@@ -104,58 +98,48 @@ def models(latitude: float, longitude: float):
         dataframe.replace({None: 0}, inplace=True)
         dataframe.reset_index(drop=True, inplace=True)
 
-    size_train = int(data.shape[0]*0.8)
-    size_data = int(prediction_data.shape[0]) - 1
     train_data = data.dropna().copy()
-    test_data = prediction_data.dropna().copy()
+    test_data = prediction_test.dropna().copy()
 
     # /----- Temperature Prediction Train -----/
-
     modelTemp = LinearRegression()
-
-    x_train = train_data.iloc[:size_train, :].drop(columns=["target_temperature"])
-    x_test  = train_data.iloc[size_train:, :].drop(columns=["target_temperature"])
-    y_train = train_data["target_temperature"].iloc[:size_train]
-    y_test  = train_data["target_temperature"].iloc[size_train:]
-
+    x_train = train_data.drop(columns=["target_temperature", "target_weather"])
+    y_train = train_data["target_temperature"]
     modelTemp.fit(x_train, y_train)
-  
-    predT = modelTemp.predict(x_test)
-    margin = root_mean_squared_error(y_test, predT)
 
     # /----- Weather Prediction Train -----/
-
     counts  = data["weather_code"].value_counts()
     weights = {weight: counts.sum()/count for weight, count in counts.items()}
-
-    modelWeather = RandomForestClassifier(class_weight=weights, random_state=67,)
-
-    x_train = train_data.iloc[:size_train, 1:]
-    y_train = train_data.iloc[:size_train, 0]
-    x_test  = train_data.iloc[size_train:, 1:]
-    y_test  = train_data.iloc[size_train:, 0]
-
+    modelWeather = RandomForestClassifier(class_weight=weights, random_state=67)
+    x_train = train_data.drop(columns=["weather_code", "target_temperature", "target_weather"])
+    y_train = train_data["weather_code"]
     modelWeather.fit(x_train, y_train)
 
-    predW = modelWeather.predict(x_test)
+    # /----- Testing model accuracy -----/
+    x_test  = test_data.drop(columns=["target_temperature", "target_weather"])
+    y_test  = test_data["target_temperature"]
+    predT   = modelTemp.predict(x_test)
+    margin  = root_mean_squared_error(y_test, predT)
+
+    x_test  = test_data.drop(columns=["weather_code", "target_temperature", "target_weather"])
+    y_test  = test_data["weather_code"]
+    predW   = modelWeather.predict(x_test)
     precision = precision_score(y_test, predW, average='weighted')
 
-    del train_data
-    del test_data
-
     # /----- Temperature tomorrow's prediction -----/
-
+    x_predT = prediction_data.drop(columns=["target_temperature", "target_weather"])
+    predT   = modelTemp.predict(x_predT.tail(1))
 
     # /----- Weather tomorrow's prediction -----/
+    x_predW = prediction_data.drop(columns=["weather_code", "target_temperature", "target_weather"])
+    predW   = modelWeather.predict(x_predW.tail(1))
 
-
-    # Celsius como unidade de medida para temperatura.
-    # Precision já está convertido para porcentagem.
-    
-    return {"temperatures": int(predT[-1]), 
-            "margin": ceil(margin), 
-            "weather": weather_code[predW[-1]], 
-            "precision": round(precision * 100, 2)}
+    return {
+        "temperatures": int(predT[-1]), 
+        "margin": ceil(margin), 
+        "weather": weather_code[predW[-1]], 
+        "precision": round(precision * 100, 2)
+    }
 
 def main():
     results = models(0, 0)
